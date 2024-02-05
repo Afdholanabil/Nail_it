4ae6c8af17fb3620d1305be2ba12698121f8bee5
Afdholanabil <afdholanabil41435@gmail.com>
2024-02-05 09:51:29 +0700

    Penambahan register dan lengkapi data diri

---
+++ app/src/main/java/com/example/nail_it/LengkapiDataDiriActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nail_it
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LengkapiDataDiriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lengkapi_data_diri)
+    }
+}
