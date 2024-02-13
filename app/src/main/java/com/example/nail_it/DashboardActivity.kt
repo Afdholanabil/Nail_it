f4bb0bf5a9a63b1a30fe2b32c395ea480c4709a9
Afdholanabil <afdholanabil41435@gmail.com>
2024-02-13 11:06:47 +0700

    penyelesaian dashboard

--- app/src/main/java/com/example/nail_it/DashboardActivity.kt
+++ app/src/main/java/com/example/nail_it/DashboardActivity.kt
@@ -1,7 +1,9 @@
 package com.example.nail_it
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.nail_it.databinding.ActivityDashboardBinding
 
 class DashboardActivity : AppCompatActivity() {
@@ -10,5 +12,37 @@ class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bvBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.item_home->replaceFragment(HomeFragment())
+                R.id.item_challange->replaceFragment(ChallangeFragment())
+                R.id.item_interview->replaceFragment(InterviewFragment())
+                R.id.item_work->replaceFragment(JobsFragment())
+                R.id.item_profiles->replaceFragment(ProfileFragment())
+
+                else->{
+
+                }
+
+
+        }
+
+            true
+        }
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction  = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_fragment, fragment)
+        fragmentTransaction.commit()
+
     }
 }
