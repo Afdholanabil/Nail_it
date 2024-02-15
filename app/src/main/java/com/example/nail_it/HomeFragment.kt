127dc654f6916bac2cf28643180acf8e1bb985b0
Afdholanabil <afdholanabil41435@gmail.com>
2024-02-15 20:36:44 +0700

    penambahan adapter slider

--- app/src/main/java/com/example/nail_it/HomeFragment.kt
+++ app/src/main/java/com/example/nail_it/HomeFragment.kt
@@ -1,10 +1,18 @@
 package com.example.nail_it
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.example.nail_it.adapter.ImageAdapter
+import kotlin.math.abs
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,13 +29,57 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var viewPager2: ViewPager2
+    private lateinit var handler: Handler
+    private lateinit var imageList:ArrayList<Int>
+    private lateinit var adapter: ImageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        setUpTransformer()
+        viewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                handler.removeCallbacks(runnable)
+                handler.postDelayed(runnable, 2000)
+            }
+        })
+
+
     }
+
+    private val runnable = Runnable {
+        viewPager2.currentItem = viewPager2.currentItem + 1
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        handler.postDelayed(runnable, 2000)
+    }
+
+    private fun setUpTransformer() {
+        val trasformer = CompositePageTransformer()
+        trasformer.addTransformer(MarginPageTransformer(40))
+        trasformer.addTransformer{ page ,position ->
+            val r = 1 - abs(position)
+            page.scaleY = 0.85f + r + 0.14f
+
+        }
+        viewPager2.setPageTransformer(trasformer)
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +87,30 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewPager2 = view.findViewById(R.id.viewpager_slider)
+        handler = Handler(Looper.myLooper()!!)
+        imageList = ArrayList()
+
+        imageList.add(R.drawable.cta_v1)
+        imageList.add(R.drawable.cta_v1)
+        imageList.add(R.drawable.cta_v1)
+        imageList.add(R.drawable.cta_v1)
+        imageList.add(R.drawable.cta_v1)
+
+        adapter = ImageAdapter(imageList, viewPager2)
+
+        viewPager2.adapter = adapter
+        viewPager2.offscreenPageLimit = 1
+        viewPager2.clipToPadding = false
+        viewPager2.clipChildren = false
+        viewPager2.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
     }
 
     companion object {
---
+++ app/src/main/java/com/example/nail_it/adapter/ImageAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.nail_it.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
+import com.example.nail_it.R
+
+class ImageAdapter(private val imageList : ArrayList<Int>, private val viewPager2: ViewPager2)
+
+    : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val imageView : ImageView = itemView.findViewById(R.id.iv_slider);
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.image_container, parent, false)
+        return  ImageViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return imageList.size
+    }
+
+    private val runnable = Runnable{
+        imageList.addAll(imageList)
+        notifyDataSetChanged()
+
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(imageList[position])
+
+        if (position == imageList.size-1){
+            viewPager2.post(runnable)
+
+        }
+    }
+}
